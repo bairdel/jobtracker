a17b472c7cc6730952ea106148c095c6ba3f3750
bairdel <bairdel@outlook.com>
2025-04-14 20:14:04 +0100

    try with setting python output to var

--- checkchanges.py
+++ checkchanges.py
@@ -1,5 +1,7 @@
 import urllib.request
 from urllib.parse import urlparse
+# import os
+
 
 with open("./websitesToCheck.txt", "r") as f:
     websites = f.readlines()
@@ -13,3 +15,9 @@ for w in websites:
     with open("./latest-content/"+ name + ".html", "wb") as f:
         f.write(page.read())
 print("sending this to you in an email")
+
+
+# env_file = os.getenv('GITHUB_ENV')
+
+# with open(env_file, "a") as myfile:
+#     myfile.write("email=MY_VALUE")
