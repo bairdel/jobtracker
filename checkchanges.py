70cd4c9707d141cdaa4775da92d69c022bea1970
bairdel <bairdel@outlook.com>
2025-04-15 15:52:46 +0100

    going to try with some formatting using bs4

--- checkchanges.py
+++ checkchanges.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup as bs4
+from bs4 import Comment
 import difflib
 
 # import os
@@ -26,15 +27,33 @@ for w in websites:
         with open("./latest-content/"+ name + ".html", "r", encoding='utf-8') as f:
             saved_page = f.read()
         soup = bs4(page.content, 'html.parser')
-        body = soup.find("body").text.strip()
+        comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+        for comment in comments:
+            comment.extract()
 
+        body = soup.find("body")#.text.strip()
+
+        first = []
+        for p in saved_page.splitlines():
+            soup = bs4(p, 'html.parser')
+            first.append(soup.text)
+
+        second = []
+
+        for p in str(body).splitlines():
+            soup = bs4(p, 'html.parser')
+            second.append(soup.text)
+        # all_tags = soup.find_all()
+        # for tag in all_tags:
+        #     print(tag.get_text())
+        #     print()
         # compare the two versions of the page
-        diff = difflib.unified_diff(saved_page.splitlines(), str(body).splitlines(), fromfile='saved_page', tofile='str(body)', lineterm='', n=0)
+        diff = difflib.unified_diff(first, second, fromfile='saved_page', tofile='str(body)', lineterm='', n=0)
         lines = list(diff)[2:]
         added = [line[1:] for line in lines if line[0] == '+']
         removed = [line[1:] for line in lines if line[0] == '-']
 
-        additions = ""
+        additions =[]
 
         # print('additions:')
         # for line in added:
@@ -44,19 +63,23 @@ for w in websites:
         for line in added:
             if line not in removed:
                 # print(line)
-                additions += line + "\n"
+                additions.append(line)
 
         changed[w] = additions
 
 
         # replace html with newest version
         with open("./latest-content/"+ name + ".html", "w", encoding='utf-8') as f:
-            f.write(body)
+            f.write(str(body))
     else:
         print("not found")
         # print(page.read())
         soup = bs4(page.content, 'html.parser')
-        body = soup.find("body").text.strip()
+        comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+        for comment in comments:
+            comment.extract()
+
+        body = soup.find("body")#.text.strip()
         with open("./latest-content/"+ name + ".html", "w", encoding='utf-8') as f:
             f.write(str(body))
 
@@ -69,11 +92,14 @@ for w in websites:
 # for key, value in changed.items():
 #     print(f"{key}: {value}")
 #     print("   ")
+print(changed)
 
-with open("./emailcontent/email.txt", "w", encoding='utf-8') as f:
+with open("./emailcontent/email.html", "w", encoding='utf-8') as f:
     f.write("this is the email now\n")
     for key, value in changed.items():
-        f.write(f"{key}: {value}")
+        f.write(f"<h2>{key}</h2>")
+        for items in value:
+            f.write('<p>%s</p><br>' %items)
         f.write("   ")
 # env_file = os.getenv('GITHUB_ENV')
 
