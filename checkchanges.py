5ddd00c9c7d1df87210b9fe77b3d86f50480a8dd
bairdel <bairdel@outlook.com>
2025-04-21 22:30:28 +0100

    possibly the website doesn't like the connection

--- checkchanges.py
+++ checkchanges.py
@@ -15,77 +15,82 @@ with open("./websitesToCheck.txt", "r") as f:
 changed = {}
 # print(websites)
 for w in websites:
-    page = urllib.request.urlopen(w)
-    page = requests.get(w)
-    name = urlparse(w).netloc
+    try: 
+        page = urllib.request.urlopen(w)
+        page = requests.get(w)
+        name = urlparse(w).netloc
 
-    my_file = Path("./latest-content/"+ name + ".html")
-    if my_file.is_file():
-        print("requesting " + name)
-        # compare
 
-        # open saved version of the page
-        with open("./latest-content/"+ name + ".html", "r", encoding='utf-8') as f:
-            saved_page = f.read()
-        soup = bs4(page.content, 'html.parser')
-        comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-        for comment in comments:
-            comment.extract()
+        my_file = Path("./latest-content/"+ name + ".html")
+        if my_file.is_file():
+            print("requesting " + name)
+            # compare
 
-        body = soup.find("body")#.text.strip()
+            # open saved version of the page
+            with open("./latest-content/"+ name + ".html", "r", encoding='utf-8') as f:
+                saved_page = f.read()
+            soup = bs4(page.content, 'html.parser')
+            comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+            for comment in comments:
+                comment.extract()
 
-        first = []
-        for p in saved_page.splitlines():
-            soup = bs4(p, 'html.parser')
-            first.append(soup.text)
+            body = soup.find("body")#.text.strip()
 
-        second = []
+            first = []
+            for p in saved_page.splitlines():
+                soup = bs4(p, 'html.parser')
+                first.append(soup.text)
 
-        for p in str(body).splitlines():
-            soup = bs4(p, 'html.parser')
-            second.append(soup.text)
-        # all_tags = soup.find_all()
-        # for tag in all_tags:
-        #     print(tag.get_text())
-        #     print()
-        # compare the two versions of the page
+            second = []
 
-        diff = difflib.unified_diff(first, second, fromfile='saved_page', tofile='str(body)', lineterm='', n=0)
-        lines = list(diff)[2:]
-        added = [line[1:] for line in lines if line[0] == '+']
-        removed = [line[1:] for line in lines if line[0] == '-']
+            for p in str(body).splitlines():
+                soup = bs4(p, 'html.parser')
+                second.append(soup.text)
+            # all_tags = soup.find_all()
+            # for tag in all_tags:
+            #     print(tag.get_text())
+            #     print()
+            # compare the two versions of the page
 
-        additions =[]
-        print(len(lines))
+            diff = difflib.unified_diff(first, second, fromfile='saved_page', tofile='str(body)', lineterm='', n=0)
+            lines = list(diff)[2:]
+            added = [line[1:] for line in lines if line[0] == '+']
+            removed = [line[1:] for line in lines if line[0] == '-']
 
-        # print('additions:')
-        # for line in added:
-        #     print(line)
-        # print("------------")
-        # print('additions, ignoring position')
-        for line in added:
-            if line not in removed:
-                # print(line)
-                additions.append(line)
-        # print(additions)
-        if len(additions) != 0:
-            changed[w] = additions
+            additions =[]
+            print(len(lines))
 
-        # replace html with newest version
-        with open("./latest-content/"+ name + ".html", "w", encoding='utf-8') as f:
-            print("writing to " + name)
-            f.write(str(body))
-    else:
-        print(name + "not found")
-        # print(page.read())
-        soup = bs4(page.content, 'html.parser')
-        comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-        for comment in comments:
-            comment.extract()
+            # print('additions:')
+            # for line in added:
+            #     print(line)
+            # print("------------")
+            # print('additions, ignoring position')
+            for line in added:
+                if line not in removed:
+                    # print(line)
+                    additions.append(line)
+            # print(additions)
+            if len(additions) != 0:
+                changed[w] = additions
 
-        body = soup.find("body")#.text.strip()
-        with open("./latest-content/"+ name + ".html", "w", encoding='utf-8') as f:
-            f.write(str(body))
+            # replace html with newest version
+            with open("./latest-content/"+ name + ".html", "w", encoding='utf-8') as f:
+                print("writing to " + name)
+                f.write(str(body))
+        else:
+            print(name + "not found")
+            # print(page.read())
+            soup = bs4(page.content, 'html.parser')
+            comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+            for comment in comments:
+                comment.extract()
+
+            body = soup.find("body")#.text.strip()
+            with open("./latest-content/"+ name + ".html", "w", encoding='utf-8') as f:
+                f.write(str(body))
+
+    except Exception as e:
+        print(e)
 
 
 # print("sending this to you in an email")
